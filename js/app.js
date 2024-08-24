3765e88c0de297cd0d909566ed7a74bec5fcf511
Max <primmax9@gmail.com>
2024-08-24 19:13:02 +0300

    Info button added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const slides = document.querySelectorAll('.slide')
+const icon = document.querySelector('.info__icon')
+const descr = document.querySelector('.info__descr')
+const container = document.querySelector('.container')
 
 for (const slide of slides) {
 	slide.addEventListener('click', () => {
@@ -13,3 +16,13 @@ function clearActiveClasses() {
 		slide.classList.remove('active')
 	})
 }
+
+icon.addEventListener('mouseenter', ()=> {
+	container.classList.add('transparent')
+	setTimeout(function() {descr.classList.add('displayed')},400)
+})
+
+icon.addEventListener('mouseleave', ()=> {
+	descr.classList.remove('displayed')
+	setTimeout(function() {container.classList.remove('transparent')},400)
+})
